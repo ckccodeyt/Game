f66ef222f76a71260c7860a5be95685abfdcd655
ckccodeyt <ckclol1434@gmail.com>
2026-01-22 12:26:37 +0100

    Switch from pygame to turtle for input handling
    Replace pygame with turtle graphics for movement controls.

--- Index.py
+++ Index.py
@@ -1,1 +1,11 @@
-import pygame
+import turtle 
+Turtle.trutle("t")
+f = input("WASD")
+if f == "W" or "w"
+  t.forward(5) 
+if f == "a" or "A"
+  t.left(5)
+if f == "s" or "S"
+  t.back(5)
+if f == "d" or "D"
+  t.left(-5)
